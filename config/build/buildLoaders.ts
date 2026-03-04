7471882ef437b3ac893065289039b6a743935250
Denys Skulimovskyi <denys@definely.com>
2026-03-04 18:39:39 +0400

    add react & css-loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,27 @@
 import * as webpack from 'webpack';
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+  const typescriptLoader = {
+    // Use tsx is enought for using React
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      "style-loader",
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
   return [
-    {
-      test: /\.tsx?$/,
-      use: "ts-loader",
-      exclude: /node_modules/,
-    },
+    typescriptLoader,
+    cssLoader,
   ];
 }
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { someFn } from "./test";
-
-someFn(123);
-
-document.body.innerHTML = '<h1>Hello, World!</h1>';
