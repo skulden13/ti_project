8735e0ef45188659e63ead22d94e5fb388ed8349
Denys Skulimovskyi <denys@definely.com>
2026-03-20 12:55:51 +0400

    fix builds

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -7,12 +7,9 @@ import MainPage from './MainPage';
 export default {
   title: 'pages/MainPage',
   component: MainPage,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof MainPage>;
 
-const Template: ComponentStory<typeof MainPage> = (args) => <MainPage {...args} />;
+const Template: ComponentStory<typeof MainPage> = () => <MainPage />;
 
 export const Normal = Template.bind({});
 Normal.args = {};
