12935a513245b09d20101b294f07bd60d937b62a
Denys Skulimovskyi <denys@definely.com>
2026-03-20 12:38:58 +0400

    config: re-use buildCssLoaders fn

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import * as webpack from 'webpack';
 import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   const babelLoader = {
@@ -44,38 +44,20 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  const cssModuleLoader = {
-    test: /\.module\.s[ac]ss$/i,
-    use: [
-      options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-      {
-        loader: 'css-loader',
-        options: {
-          esModule: false,
-          modules: {
-            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-            localIdentName: options.isDev
-              ? '[path][name]__[local]'
-              : '[hash:base64:8]',
-          },
-        },
-      },
-      'sass-loader',
-    ],
-  };
+  const cssModuleLoader = buildCssLoader(options.isDev);
 
-  const cssLoader = {
-    test: /\.s[ac]ss$/i,
-    exclude: /\.module\.s[ac]ss$/i,
-    use: [
-      // Creates `style` nodes from JS strings
-      options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-      // Translates CSS into CommonJS
-      'css-loader',
-      // Compiles Sass to CSS
-      'sass-loader',
-    ],
-  };
+  // const cssLoader = {
+  //   test: /\.s[ac]ss$/i,
+  //   exclude: /\.module\.s[ac]ss$/i,
+  //   use: [
+  //     // Creates `style` nodes from JS strings
+  //     options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+  //     // Translates CSS into CommonJS
+  //     'css-loader',
+  //     // Compiles Sass to CSS
+  //     'sass-loader',
+  //   ],
+  // };
 
   return [
     fileLoader,
@@ -83,6 +65,6 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     babelLoader,
     typescriptLoader,
     cssModuleLoader,
-    cssLoader,
+    // cssLoader,
   ];
 }
