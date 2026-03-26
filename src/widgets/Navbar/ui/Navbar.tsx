88d22aee44666a4d54eb90b2a9072bc58df4df80
Denys Skulimovskyi <denys@definely.com>
2026-03-26 16:44:14 +0400

    Sidebar: move links from Navbar, add icons & upgrade Sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,4 @@
 import { classNames } from 'shared/lib';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
 
 import cls from './Navbar.module.scss';
 
@@ -8,27 +6,10 @@ interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-  const { t: t1 } = useTranslation('main');
-  const { t: t2 } = useTranslation('about');
-
-  return (
-    <div className={classNames(cls.navbar, {}, className ? [className] : [])}>
-      <div className={cls.links}>
-        <AppLink
-          to="/"
-          className={cls.mainLink}
-          theme={AppLinkTheme.SECONDARY}
-        >
-          {t1('Main')}
-        </AppLink>
-        <AppLink
-          to="/about"
-          theme={AppLinkTheme.SECONDARY}
-        >
-          {t2('About')}
-        </AppLink>
-      </div>
+export const Navbar = ({ className }: NavbarProps) => (
+  <div className={classNames(cls.navbar, {}, className ? [className] : [])}>
+    <div className={cls.links}>
+      /
     </div>
-  );
-};
+  </div>
+);
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,11 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
+import { RoutePaths } from 'shared/config/routeConfig/routeConfig';
+import AboutIcon from 'shared/assets/icons/about-20-20.svg';
+import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -11,6 +16,8 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false);
+  const { t: t1 } = useTranslation('main');
+  const { t: t2 } = useTranslation('about');
 
   const onToggle = () => {
     setCollapsed((prev) => !prev);
@@ -37,6 +44,34 @@ export const Sidebar = ({ className }: SidebarProps) => {
       >
         {collapsed ? '>' : '<'}
       </Button>
+
+      <div className={cls.items}>
+        <div className={cls.item}>
+          <AppLink
+            to={RoutePaths.main}
+            theme={AppLinkTheme.SECONDARY}
+            className={cls.link}
+          >
+            <MainIcon className={cls.icon} />
+            <span className={cls.text}>
+              {t1('Main')}
+            </span>
+          </AppLink>
+        </div>
+        <div className={cls.item}>
+          <AppLink
+            to={RoutePaths.about}
+            theme={AppLinkTheme.SECONDARY}
+            className={cls.link}
+          >
+            <AboutIcon className={cls.icon} />
+            <span className={cls.text}>
+              {t2('About')}
+            </span>
+          </AppLink>
+        </div>
+      </div>
+
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
