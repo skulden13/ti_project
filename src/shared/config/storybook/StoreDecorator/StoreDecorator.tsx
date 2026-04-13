67dfb7704c547b172d8eee4042bd4138c2100aae
Denys Skulimovskyi <denys@definely.com>
2026-04-13 17:07:31 +0400

    stories: upd store decorators to accept async reducers

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,11 +1,20 @@
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+  loginForm: loginReducer,
+};
 
 export const StoreDecorator = (
-  initialState: DeepPartial<StateSchema> = {},
+  initialState: DeepPartial<StateSchema>,
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
 ) => (StoryComponent: Story) => (
-  <StoreProvider initialState={initialState}>
+  <StoreProvider
+    initialState={initialState}
+    asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
+  >
     <StoryComponent />
   </StoreProvider>
 );
