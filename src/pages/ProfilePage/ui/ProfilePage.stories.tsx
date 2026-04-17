9c3310e666e135d0a7432605869dc93410600b86
Denys Skulimovskyi <denys@definely.com>
2026-04-17 19:49:07 +0400

    ui tests: upd ProfilePage stories

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Country, Currency } from 'shared/const/common';
 import ProfilePage from './ProfilePage';
 
 export default {
@@ -11,10 +12,28 @@ export default {
 
 const Template: ComponentStory<typeof ProfilePage> = () => <ProfilePage />;
 
+const profileState = {
+  profile: {
+    data: {
+      firstname: 'Denis',
+      lastname: 'Skulimovskiy',
+      age: 25,
+      currency: Currency.USD,
+      country: Country.Georgia,
+      city: 'Tbilisi',
+      username: 'skulden13',
+      avatar: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    },
+    isLoading: false,
+    error: undefined,
+    readOnly: true,
+  },
+};
+
 export const Normal = Template.bind({});
 Normal.args = {};
-Normal.decorators = [StoreDecorator({})];
+Normal.decorators = [StoreDecorator(profileState)];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator(profileState)];
