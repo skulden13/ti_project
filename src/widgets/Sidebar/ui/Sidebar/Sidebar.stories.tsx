74ff008c871d07994d161cf613a6454c53ab51a0
Denys Skulimovskyi <denys@definely.com>
2026-04-27 17:43:30 +0400

    loki: upd screenshots

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -2,8 +2,8 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Sidebar } from './Sidebar';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Sidebar } from './Sidebar';
 
 export default {
   title: 'widgets/Sidebar',
