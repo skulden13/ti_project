f253888cce5f03aef60a451c6e653346611a9069
Denys Skulimovskyi <denys@definely.com>
2026-05-15 16:41:12 +0400

    Article Details Page: add CommentsList & data fetch to ui

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -3,22 +3,53 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { classNames } from 'shared/lib';
+import { CommentList } from 'entities/Comment';
+import { DynamicModuleLoader, ReducersList }
+  from 'shared/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useSelector } from 'react-redux';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { articleDetailsCommentsReducer, getArticleComments }
+  from '../../model/slice/articleDetailsCommentsSlice';
 import cls from './ArticleDetailsPage.module.scss';
+import { getArticleCommentsIsLoading } from '../../model/selectors/comments';
+import { fetchCommentsByArticleId }
+  from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
 
 interface ArticleDetailsPageProps {
   className?: string;
 }
+const reducers: ReducersList = {
+  articleDetailsComments: articleDetailsCommentsReducer,
+};
 
 const ArticleDetailsPage = memo(({ className }: ArticleDetailsPageProps) => {
   const { t } = useTranslation('article');
   const { id } = useParams<{ id: string }>();
+  const dispatch = useAppDispatch();
+  const comments = useSelector(getArticleComments.selectAll);
+  const isLoading = useSelector(getArticleCommentsIsLoading);
 
-  const content = !id ? t('ArticleNotFound') : <ArticleDetails id={id} />;
+  useInitialEffect(() => {
+    dispatch(fetchCommentsByArticleId(id));
+  });
+
+  const content = !id ? t('ArticleNotFound') : (
+    <>
+      <ArticleDetails id={id} />
+      <CommentList
+        isLoading={isLoading}
+        comments={comments}
+      />
+    </>
+  );
 
   return (
-    <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-      {content}
-    </div>
+    <DynamicModuleLoader reducers={reducers}>
+      <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+        {content}
+      </div>
+    </DynamicModuleLoader>
   );
 });
 
