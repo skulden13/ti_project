b02a52e615244e8000136d860f4cd7a822e5544e
Denys Skulimovskyi <denys@definely.com>
2026-05-27 17:08:17 +0400

    storybook: uncomment stories for AddCommentForm

--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.stories.tsx
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.stories.tsx
@@ -1,26 +1,27 @@
-// import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-// import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
-// import AddCommentForm from './AddCommentForm';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { action } from '@storybook/addon-actions';
+import AddCommentForm from './AddCommentForm';
 
-// export default {
-//   title: 'features/addCommentForm',
-//   component: AddCommentForm,
-//   argTypes: {
-//     backgroundColor: { control: 'color' },
-//   },
-// } as ComponentMeta<typeof AddCommentForm>;
+export default {
+  title: 'features/AddCommentForm',
+  component: AddCommentForm,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof AddCommentForm>;
 
-// const Template: ComponentStory<typeof AddCommentForm> = (args) => <AddCommentForm {...args} />;
+const Template: ComponentStory<typeof AddCommentForm> = (args) => <AddCommentForm {...args} />;
 
-// export const Primary = Template.bind({});
-// Primary.args = {};
-// Primary.decorators = [StoreDecorator({
-//   addCommentForm: { text: 'some text' },
-// })];
+export const Primary = Template.bind({});
+Primary.args = {
+  onSendComment: action('onSendComment'),
+};
+Primary.decorators = [StoreDecorator({})];
 
-// export const WithError = Template.bind({});
-// WithError.args = {};
-// WithError.decorators = [StoreDecorator({
-//   addCommentForm: { error: 'Some error' },
-// })];
+export const WithError = Template.bind({});
+WithError.args = {};
+WithError.decorators = [StoreDecorator({
+  addCommentForm: { error: 'Some error' },
+})];
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -2,13 +2,18 @@ import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { articleDetailsReducer } from 'entities/Article/model/slice/articleDetailsSlice';
 import { profileReducer } from 'entities/Profile';
+import { addCommentReducer } from 'features/addCommentForm/model/slice/addCommentFormSlice';
 import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+import { articleDetailsCommentsReducer }
+  from 'pages/ArticleDetailsPage/model/slice/articleDetailsCommentsSlice';
 import { ReducersList } from 'shared/components/DynamicModuleLoader/DynamicModuleLoader';
 
 const defaultAsyncReducers: ReducersList = {
   loginForm: loginReducer,
   profile: profileReducer,
   articleDetails: articleDetailsReducer,
+  addCommentForm: addCommentReducer,
+  articleDetailsComments: articleDetailsCommentsReducer,
 };
 
 export const StoreDecorator = (
