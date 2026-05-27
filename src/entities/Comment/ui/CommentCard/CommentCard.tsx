8dcc12245d0bf3cf10ece918b2bfebdd63371cae
Denys Skulimovskyi <denys@definely.com>
2026-05-27 17:10:26 +0400

    CommentCard: make comment param optionla, return null if it's undefined

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -11,40 +11,37 @@ import { Comment } from '../../model/types/comment';
 
 interface CommentCardProps {
   className?: string;
-  comment: Comment;
+  comment?: Comment;
   isLoading?: boolean;
 }
 
 export const CommentCard = memo((props: CommentCardProps) => {
   const { className, comment, isLoading } = props;
-  const { user } = comment;
+  const user = comment?.user;
 
-  let header;
-  let text;
   if (isLoading) {
-    header = (
-      <>
-        <Skeleton width={30} height={30} border="50%" />
-        <Skeleton className={cls.username} width={100} height={16} />
-      </>
+    return (
+      <div className={classNames(cls.CommentCard, {}, [className, cls.loading])}>
+        <AppLink to={`${RoutePaths.profile}${user?.id}`} className={cls.header}>
+          <Skeleton width={30} height={30} border="50%" />
+          <Skeleton className={cls.username} width={100} height={16} />
+        </AppLink>
+        <Skeleton width="100%" height={50} />
+      </div>
     );
-    text = <Skeleton width="100%" height={50} />;
-  } else {
-    header = (
-      <>
-        {user.avatar ? <Avatar size={30} src={user.avatar} /> : null}
-        <Text className={cls.username} title={user.username} />
-      </>
-    );
-    text = <Text text={comment.text} />;
+  }
+
+  if (!comment) {
+    return null;
   }
 
   return (
     <div className={classNames(cls.CommentCard, {}, [className])}>
-      <AppLink to={`${RoutePaths.profile}${user.id}`} className={cls.header}>
-        {header}
+      <AppLink to={`${RoutePaths.profile}${user?.id}`} className={cls.header}>
+        {user?.avatar ? <Avatar size={30} src={user.avatar} /> : null}
+        <Text className={cls.username} title={user?.username} />
       </AppLink>
-      {text}
+      <Text text={comment?.text} />
     </div>
   );
 });
