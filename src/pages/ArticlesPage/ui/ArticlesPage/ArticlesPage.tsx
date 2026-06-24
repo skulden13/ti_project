f5f43fc76fa66ebf53afcf29bab543df0adefe7a
Denys Skulimovskyi <denys@definely.com>
2026-06-24 14:50:55 +0400

    ArticlesPage: use data from db

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,18 +1,45 @@
-import { ArticleList, ArticleView } from 'entities/Article';
+import { ArticleList } from 'entities/Article';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { DynamicModuleLoader, ReducersList }
+  from 'shared/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useInitialEffect }
+  from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { articlesPageReducer, getArticles } from '../../model/slice/articlesPageSlice';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
+import { getArticlePageError, getArticlePageIsLoading, getArticlePageView }
+  from '../../model/selectors/articlePageSelectors';
+
+const reducers: ReducersList = {
+  articlesPage: articlesPageReducer,
+};
 
 const ArticlesPage = memo(() => {
   const { t } = useTranslation('article');
+  const dispatch = useAppDispatch();
+  const articles = useSelector(getArticles.selectAll);
+  const isLoading = useSelector(getArticlePageIsLoading);
+  const error = useSelector(getArticlePageError);
+  const view = useSelector(getArticlePageView);
+
+  useInitialEffect(() => {
+    dispatch(fetchArticlesList());
+  });
 
   return (
-    <div>
-      <h1>{t('ArticlesPage')}</h1>
-      <ArticleList
-        view={ArticleView.EXPANDED}
-        articles={[]}
-      />
-    </div>
+    <DynamicModuleLoader reducers={reducers}>
+      <div>
+        <h1>{t('ArticlesPage')}</h1>
+        <ArticleList
+          articles={articles}
+          view={view}
+          isLoading={isLoading}
+          // error={error}
+        />
+      </div>
+    </DynamicModuleLoader>
   );
 });
 
