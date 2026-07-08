9a5114a62856e3cbe30933b0748747376cea1813
Denys Skulimovskyi <denys@definely.com>
2026-07-08 14:04:04 +0400

    refactoring: more Page component to widgets

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -7,7 +7,7 @@ import { useInitialEffect }
   from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useSelector } from 'react-redux';
-import { Page } from 'shared/ui/Page/Page';
+import { Page } from 'widgets/Page/Page';
 import { articlesPageActions, articlesPageReducer, getArticles }
   from '../../model/slice/articlesPageSlice';
 import {
--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib';
 import { memo } from 'react';
-import { Page } from 'shared/ui/Page/Page';
+import { Page } from 'widgets/Page/Page';
 import cls from './NotFoundPage.module.scss';
 
 interface NotFoundPageProps {
