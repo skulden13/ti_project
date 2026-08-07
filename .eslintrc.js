ed5ae27b44dc3a4da78486f4a72578900fc1cc8f
Denys Skulimovskyi <denys@definely.com>
2026-08-07 20:12:18 +0400

    eslint: ignore Stack props

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,7 +46,7 @@ module.exports = {
       'error',
       {
         markupOnly: true,
-        ignoreAttribute: ['data-testid', 'to', 'target'],
+        ignoreAttribute: ['data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap'],
       },
     ],
     'max-len': ['error', { code: 100, ignoreComments: true }],
